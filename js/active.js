4fbc57f0050929dc73fb49b62f0324a2c311e2e9
dada <dada@bwlohas.com>
2021-02-20 10:50:56 +0800

    new

---
+++ js/active.js
@@ -0,0 +1,11 @@
+// 给超级连接添加magnificPopup初始化方法。
+$('#lightbox').magnificPopup({
+  type: 'inline',         // 行内的类型，类比图片的模式
+  closeBtnInside: true,   // 显示关闭按钮
+  closeOnBgClick: false  // 点击遮罩透明背景关闭弹出层
+});
+$('#lightbox2').magnificPopup({
+  type: 'inline',         // 行内的类型，类比图片的模式
+  closeBtnInside: true,   // 显示关闭按钮
+  closeOnBgClick: false  // 点击遮罩透明背景关闭弹出层
+});
